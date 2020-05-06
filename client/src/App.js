00a1472157c16d88a0dcfffc500a032468f76e23
rauleflores <58542850+rauleflores@users.noreply.github.com>
2020-05-06 16:59:18 -0700

    Initial commit/ working on Part 1/ movie list renders to screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-import SavedList from './Movies/SavedList';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+import { Route } from "react-router-dom";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -10,25 +12,30 @@ const App = () => {
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
+        .get("http://localhost:5000/api/movies")
+        .then((response) => {
           setMovieList(response.data);
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch((error) => {
+          console.error("Server Error", error);
         });
-    }
+    };
     getMovies();
   }, []);
 
-  const addToSavedList = movie => {
+  const addToSavedList = (movie) => {
     setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route
+        exact
+        path="/"
+        component={() => <MovieList movies={movieList} />}
+      />
+      <Route exact path="/movies/:id" component={() => <Movie />} />
     </div>
   );
 };
